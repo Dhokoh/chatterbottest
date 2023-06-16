b6defa8432854b2687131432d39b1f3be69b091b
Dhokoh <davidhogarcia@gmail.com>
2023-06-16 16:47:53 -0500

    chatbot test more training

--- chatbot_test.py
+++ chatbot_test.py
@@ -7,31 +7,41 @@ collections.Hashable = collections.abc.Hashable
 testbot = ChatBot(
     "Test Bot", 
     logic_adapters = [
-        "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.BestMatch",
-        "chatterbot.logic.TimeLogicAdapter"
+        "chatterbot.logic.BestMatch"
 ])
 
-trainer = ListTrainer(testbot)
+clerk_convo = [
+    "Hi",
+    "Hi, how can I help you?",
+    "I need to buy shoes",
+    "Sure thing, what size would you be?",
+    "Sure, I'm 10",
+    "I'm sorry, unfortunately, we don't have that size at the moment",
+    "Oh... okay then. Thank you.",
+    "You're welcome, anything else I can do for you?",
+    "Not right now, thanks.",
+    "Alright, thank you. Goodbye.",
+    "Goodbye."
+]
+
+small_talk = [
+    "Hello",
+    "Hi!",
+    "How's the weather today?",
+    "It's cool.",
+    "Oh, by the way, what time is it?"
+]
+
+list_trainer = ListTrainer(testbot)
 
 corpus_trainer = ChatterBotCorpusTrainer(testbot)
 
-trainer.train([
-    "Yo!",
-    "Hello",
-    "All good?",
-    "Yes, how about you?",
-    "All good too. Thanks for asking. Bye",
-    "Goodbye!",
-    "How are you?",
-    "I'm fine thanks for asking. How about you?",
-    "I'm fine too!",
-    "I'm happy to hear that",
-    "Goodbye",
-    "Goodbye!"
-])
-
 corpus_trainer.train("chatterbot.corpus.english")
+corpus_trainer.train("chatterbot.corpus.spanish")
+
+
+for statement in small_talk, clerk_convo:
+    list_trainer.train(statement)
 
 while True:
     try:
