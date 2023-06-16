e356f7e2266b3dfae0e567a53165cb00647c5cbe
Dhokoh <davidhogarcia@gmail.com>
2023-06-16 11:09:05 -0500

    chatbot tests with corpus training

--- chatbot_test.py
+++ chatbot_test.py
@@ -1,12 +1,42 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+import collections.abc
 
-testbot = ChatBot("Test Bot")
+collections.Hashable = collections.abc.Hashable
+
+testbot = ChatBot(
+    "Test Bot", 
+    logic_adapters = [
+        "chatterbot.logic.MathematicalEvaluation",
+        "chatterbot.logic.BestMatch",
+        "chatterbot.logic.TimeLogicAdapter"
+])
+
+trainer = ListTrainer(testbot)
+
+corpus_trainer = ChatterBotCorpusTrainer(testbot)
+
+trainer.train([
+    "Yo!",
+    "Hello",
+    "All good?",
+    "Yes, how about you?",
+    "All good too. Thanks for asking. Bye",
+    "Goodbye!",
+    "How are you?",
+    "I'm fine thanks for asking. How about you?",
+    "I'm fine too!",
+    "I'm happy to hear that",
+    "Goodbye",
+    "Goodbye!"
+])
+
+corpus_trainer.train("chatterbot.corpus.english")
 
 while True:
     try:
-        bot_input = testbot.get_response(input())
-        print(f"User: {bot_input}")
+        bot_input = testbot.get_response(input("You: "))
+        print(f"Test Bot: {bot_input}")
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
     except (AttributeError):
